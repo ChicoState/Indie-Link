ac6f12890d5f58ad16ca356775ecf481312035ca
eavadney <eavadney@mail.csuchico.edu>
2023-10-11 12:06:48 -0700

    addition of genre model and the feature to search by a single genre

--- indielink/game/models.py
+++ indielink/game/models.py
@@ -2,11 +2,16 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+
 class Game(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
-    genre = models.CharField(max_length=100)
+    genre = models.ManyToManyField(Genre)
     description = models.TextField()
-
     def __str__(self):
         return self.name
