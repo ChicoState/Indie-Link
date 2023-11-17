fc695e35f31cf910906ec6a0095170441776c973
Hayat-White <killerpearnation@gmail.com>
2023-11-16 16:34:58 -0800

    resolved #3

--- indielink/indielink/settings.py
+++ indielink/indielink/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'landing',
     'login',
-    'game'
+    'game',
+    'profiles',
 ]
 
 MIDDLEWARE = [
---
+++ indielink/profiles/models.py
@@ -0,0 +1,12 @@
+# models.py
+from django.db import models
+from django.contrib.auth.models import User
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email = models.EmailField(blank=True, null=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
